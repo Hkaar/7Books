498a65d9e2d79f8b34f6d9f4d0d984bf233cdaa2
Hkaar <HkaarDev@gmail.com>
2024-04-01 22:28:56 +0800

    Added migrations

---
+++ database/migrations/2024_04_01_133949_book_ratings.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("book_ratings", function(Blueprint $table) {
+            $table->id();
+            $table->foreignId("book_id")->constrained("books");
+            $table->foreignId("user_id")->constrained("users");
+            $table->integer("rating");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists("book_ratings");
+    }
+};
