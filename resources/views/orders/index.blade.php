773ee713a2f67f5c67784def5b6241391dd6aacd
Hkaar <HkaarDev@gmail.com>
2024-04-07 21:27:22 +0800

    Added order views

---
+++ resources/views/orders/index.blade.php
@@ -0,0 +1,103 @@
+@extends('layouts.dashboard')
+
+@section('title', "Dashboard - Books")
+
+@section('content')
+<nav id="side-nav" class="d-none d-lg-block" data-collapsed="false">
+  <div class="nav-items">
+    <a href="#" class="nav-item side-nav-open">
+      <i class="fa-solid fa-arrow-right"></i>
+    </a>
+
+    <a href="#" class="nav-item side-nav-close">
+      <i class="fa-solid fa-arrow-left"></i>
+      <span class="nav-item-title">Close</span>
+    </a>
+
+    <a href="{{ route('users.index') }}" class="nav-item">
+      <i class="fa-solid fa-user"></i>
+      <span class="nav-item-title">Users</span>
+    </a>
+
+    <a href="" class="nav-item active">
+      <i class="fa-solid fa-list"></i>
+      <span class="nav-item-title">Orders</span>
+    </a>
+
+    <a href="{{ route('books.index') }}" class="nav-item">
+      <i class="fa-solid fa-book"></i>
+      <span class="nav-item-title">Books</span>
+    </a>
+
+    <a href="#" class="nav-item">
+      <i class="fa-solid fa-pen"></i>
+      <span class="nav-item-title">Authors</span>
+    </a>
+
+    <a href="#" class="nav-item mt-auto">
+      <i class="fa-solid fa-gear"></i>
+      <span class="nav-item-title">Settings</span>
+    </a>
+  </div>
+</nav>
+
+<div id="dashboardLeftFrame" class="container">
+  <div class="action-bar mb-3">
+    <a class="btn btn-primary" href="{{ route('orders.create') }}">Add a new order</a>
+  </div>
+  
+  <div class="table-responsive">
+    <table class="table table-striped table-bordered table-hover">
+      <thead>
+        <th scope="col" width="5%">ID</th>
+        <th scope="col" width="10%">User ID</th>
+        <th scope="col">Token</th>
+        <th scope="col" width="10%">Status</th>
+        <th scope="col" width="10%">Actions</th>
+      </thead>
+    
+      <tbody>
+        @foreach ($orders as $order)
+          <tr>
+            <td>{{ $order->id }}</td>
+            <td>{{ $order->user_id }}</td>
+            <td>{{ $order->token }}</td>
+            <td>{{ $order->status }}</td>
+    
+            <td class="action-bar">
+              <button 
+                type="button" 
+                class="btn btn-info" 
+                data-bs-target="#detailsModal" 
+                data-bs-toggle="modal" 
+                hx-get="{{ route('orders.show' . $order->id) }}" 
+                hx-target="#detailsBody" 
+                hx-swap="innerHTML"
+                >Show
+              </button>
+
+              <button 
+                type="button" 
+                class="btn btn-danger" 
+                hx-headers='{"X-CSRF-TOKEN": "{{ csrf_token() }}"}' 
+                hx-delete="{{ route('books.destroy', $order->id) }}" 
+                hx-target="closest tr" 
+                hx-swap="outerHTML"
+                >Delete
+              </button>
+
+              <a 
+                class="btn btn-secondary" 
+                href="{{ route('books.edit', $order->id) }}" 
+                >Edit
+              </a>
+            </td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+
+  {{ $orders->links() }}
+</div>
+@endsection
