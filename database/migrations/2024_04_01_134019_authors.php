c9a6d3c4881e42975610b9e138068bc629c49e6e
Hkaar <HkaarDev@gmail.com>
2024-04-08 16:12:12 +0800

    Updated author migrations

--- database/migrations/2024_04_01_134019_authors.php
+++ database/migrations/2024_04_01_134019_authors.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string("name");
             $table->string("address");
             $table->string("phone");
-            $table->string("img");
+            $table->string("img")->nullable();
         });
     }
 
