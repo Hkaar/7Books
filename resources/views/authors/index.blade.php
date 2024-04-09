a7ab141e27d1d19944afaf6c346416b639ac38f0
Hkaar <HkaarDev@gmail.com>
2024-04-09 22:30:53 +0800

    Updated author index & create views

---
+++ resources/views/authors/index.blade.php
@@ -0,0 +1,108 @@
+@extends('layouts.dashboard')
+
+@section('title', "Dashboard - Authors")
+
+@section('content')
+<nav id="side-nav" class="d-none d-md-block" data-collapsed="false">
+  <div class="nav-items">
+    <a href="#" class="nav-item side-nav-open">
+      <i class="fa-solid fa-arrow-right"></i>
+    </a>
+
+    <a href="#" class="nav-item side-nav-close">
+      <i class="fa-solid fa-arrow-left"></i>
+      <span class="nav-item-title">Close</span>
+    </a>
+
+    <a href="{{ route('users.index') }}" class="nav-item">
+      <i class="fa-solid fa-user"></i>
+      <span class="nav-item-title">Users</span>
+    </a>
+
+    <a href="{{ route('orders.index') }}" class="nav-item">
+      <i class="fa-solid fa-list"></i>
+      <span class="nav-item-title">Orders</span>
+    </a>
+
+    <a href="{{ route('books.index') }}" class="nav-item">
+      <i class="fa-solid fa-book"></i>
+      <span class="nav-item-title">Books</span>
+    </a>
+
+    <a href="#" class="nav-item active">
+      <i class="fa-solid fa-pen"></i>
+      <span class="nav-item-title">Authors</span>
+    </a>
+
+    <a href="{{ route('genres.index') }}" class="nav-item">
+      <i class="fa-solid fa-tag"></i>
+      <span class="nav-item-title">Genres</span>
+    </a>
+
+    <a href="#" class="nav-item mt-auto">
+      <i class="fa-solid fa-gear"></i>
+      <span class="nav-item-title">Settings</span>
+    </a>
+  </div>
+</nav>
+
+<div id="dashboardLeftFrame" class="container">
+  <div class="action-bar mb-3">
+    <a class="btn btn-primary" href="{{ route('authors.create') }}">Add a author</a>
+  </div>
+  
+  <div class="table-responsive">
+    <table class="table table-striped table-bordered table-hover">
+      <thead>
+        <th scope="col" width="5%">ID</th>
+        <th scope="col">Name</th>
+        <th scope="col">Address</th>
+        <th scope="col" width="15%">Phone</th>
+        <th scope="col" width="15%">Actions</th>
+      </thead>
+    
+      <tbody>
+        @foreach ($authors as $author)
+          <tr>
+            <td>{{ $author->id }}</td>
+            <td>{{ $author->name }}</td>
+            <td>{{ $author->address }}</td>
+            <td>{{ $author->phone }}</td>
+    
+            <td class="action-bar">
+              <button 
+                type="button" 
+                class="btn btn-info" 
+                data-bs-target="#detailsModal" 
+                data-bs-toggle="modal" 
+                hx-get="{{ route('authors.show', $author->id) }}" 
+                hx-target="#detailsBody" 
+                hx-swap="innerHTML"
+                >Show
+              </button>
+
+              <button 
+                type="button" 
+                class="btn btn-danger" 
+                hx-headers='{"X-CSRF-TOKEN": "{{ csrf_token() }}"}' 
+                hx-delete="{{ route('authors.destroy', $author->id) }}" 
+                hx-target="closest tr" 
+                hx-swap="outerHTML"
+                >Delete
+              </button>
+  
+              <a 
+                class="btn btn-small btn-secondary" 
+                href="{{ route('authors.edit', $author->id) }}"
+                >Edit
+              </a>
+            </td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+
+  {{ $authors->links() }}
+</div>
+@endsection
