93226e8c745d17c8a5c2a1012f6356eb124cab5d
Hkaar <HkaarDev@gmail.com>
2024-04-10 17:22:16 +0800

    Added improved type checking in js & moved modlues to a seperate directory

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,7 @@
-/** @const */
+/**
+ * @typedef {import('jquery')}
+ */
+
 const invalidLiterals = ["", "[]"];
 
 /** @type {Object.<number, number>} */
@@ -7,20 +10,21 @@ let items = {};
 /**
  * Add the selected item to the current array
  * 
- * @param {Element} card
+ * @param {Element} card - The card element
  */
 function addItem(card) {
     const itemId = Number(card.getAttribute("data-item"));
-    
-    /** @type {HTMLInputElement} */
     const itemsField = document.getElementById("items");
 
+    if (!(itemsField instanceof HTMLInputElement)) {
+        return;
+    }
+
     if (!card.classList.contains("active")) {
-        /** @type {HTMLInputElement} */
         const amountField = document.getElementById("selectInputAmount");
         let amount = 1;
 
-        if (amountField && amountField.value) {
+        if (amountField instanceof HTMLInputElement && amountField.value) {
             amount = Number(amountField.value);
         }
 
@@ -47,58 +51,57 @@ function updateItemCards() {
             card.classList.add("active");  
         }
     });
-
 }
 
 $(document).ready(() => {
     'use strict';
 
-    /** @type {HTMLInputElement|null} */
     const itemsField = document.getElementById("items");
+    const sideNav = document.querySelector("#side-nav");
 
-    if (document.querySelector("#side-nav")) {
+    $(document).on("click", ".item-card", function() {
+        addItem(this);
+    })
+
+    if (itemsField instanceof HTMLInputElement && !invalidLiterals.includes(itemsField.value)) {
+        items = JSON.parse(itemsField.value);
+    }
+
+    if (sideNav) {
         $(document).on("click", ".side-nav-close", (event) => {
-            document.querySelector("#side-nav").setAttribute("data-collapsed", "true")
+            sideNav.setAttribute("data-collapsed", "true");
         })
     
         $(document).on("click", ".side-nav-open", (event) => {
-            document.querySelector("#side-nav").setAttribute("data-collapsed", "false")
+            sideNav.setAttribute("data-collapsed", "false");
         })
-    }
-
-    if ($(".item-card")) {
-        $(document).on("click", ".item-card", function() {
-            addItem(this);
-        })
-    }
-
-    if (itemsField && !(itemsField.value in invalidLiterals)) {
-        items = JSON.parse(itemsField.value);
     }
 
     if ($('#img')) {
         $(document).on("change", "#img", function() {
             let file = this.files[0];
-            
-            /** @type {Element} */
             let preview = $('#preview');
-
+    
             if (file) {
                 let reader = new FileReader();
-
+    
                 reader.onload = (e) => {
-                    if (file.type.startsWith('image')) {
-                        let img = $('<img>').attr('src', e.target.result).addClass('img-thumbnail');
+                    /** @type {string} */
+                    // @ts-ignore
+                    let result = e.target?.result;
+
+                    if (file.type.startsWith('image') && result) {
+                        let img = $('<img>').attr('src', result).addClass('img-thumbnail');
                         preview.empty().append(img);
                     } else {
                         preview.html('Image not available');
                     }
                 };
-
+    
                 reader.readAsDataURL(file);
             } else {
                 preview.empty();
             }
         });
-    }
+    }    
 })
