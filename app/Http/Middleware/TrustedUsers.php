cd9573451cb02877ba14ce6c20d0a41407570a2c
Hkaar <HkaarDev@gmail.com>
2024-04-13 15:21:33 +0800

    Added trustedusers middleware

---
+++ app/Http/Middleware/TrustedUsers.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TrustedUsers
+{
+    private $trusted = ["admin", "operator"];
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = auth()->user();
+
+        if ($user && in_array($user->level, $this->trusted)) {
+            return $next($request);
+        }
+
+        return redirect()->route("login");
+    }
+}
