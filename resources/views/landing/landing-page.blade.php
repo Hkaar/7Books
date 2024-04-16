720d0031a4753b1a778eb4f51f9efa67cd61d38f
berayenzz <tuturihandayani11577@gmail.com>
2024-04-16 12:53:53 +0800

    aku mau ganti FAQ

--- resources/views/landing/landing-page.blade.php
+++ resources/views/landing/landing-page.blade.php
@@ -1,48 +1,3 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Seven Books</title>
-
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link rel="stylesheet" href="{{ URL::asset('css/app.css'); }}">
-</head>
-<body>
-  <nav class="navbar navbar-expand-lg">
-    <div class="container-fluid">
-      <article class="brand">
-        <h5>Seven Books</h5>
-      </article>
-      
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Browse</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">About us</a>
-          </li>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link">Contact us</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
-
-
-  <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  <script defer src="{{ URL::asset('js/app.js'); }}"></script>
-</body>
-</html> -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -54,7 +9,7 @@
 <body>
     <nav>
         <section class="logo">
-        <img src="{{ URL::asset('assets/imgs/logo.png') }}" alt="">
+            <img src="sevbooks-logo (1).png" alt="">
             <h2>Seven Books</h2>
         </section>
         <ul>
@@ -80,7 +35,7 @@
             </section>
         </div>
         <section class="hero-brand">
-        <img src="{{ URL::asset('assets/imgs/7books.png') }}" alt="">
+            <img src="7books.png" alt="">
         </section>
     </section>
     <section class="about">
@@ -102,38 +57,38 @@
         <h3>Curently Trending</h3>
         <div class="books-slide">
             <div class="book">
-            <img src="{{ URL::asset('assets/imgs/book.jpg') }}" alt="">
+                <img src="book.jpg" alt="">
                 <h4>Harry Potter</h4>
             </div>
                 <div class="book">
-                <img src="{{ URL::asset('assets/imgs/book.jpg') }}" alt="">
+                    <img src="book.jpg" alt="">
                     <h4>Lord of the Rings</h4>
                 </div>
                 <div class="book">
-                <img src="{{ URL::asset('assets/imgs/book.jpg') }}" alt="">
+                    <img src="book.jpg" alt="">
                     <h4>Dungeons & Dragons</h4>
                 </div>
             <div class="book">
-            <img src="{{ URL::asset('assets/imgs/book.jpg') }}" alt="">
+                <img src="book.jpg" alt="">
                 <h4>Florida Man</h4>
             </div>
         </div>
         <h3>Newly Released</h3>
         <div class="books-slide">
             <div class="book">
-            <img src="{{ URL::asset('assets/imgs/book.jpg') }}" alt="">
+                <img src="book.jpg" alt="">
                 <h4>Harry Potter</h4>
             </div>
                 <div class="book">
-                <img src="{{ URL::asset('assets/imgs/book.jpg') }}" alt="">
+                    <img src="book.jpg" alt="">
                     <h4>Lord of the Rings</h4>
                 </div>
                 <div class="book">
-                <img src="{{ URL::asset('assets/imgs/book.jpg') }}" alt="">
+                    <img src="book.jpg" alt="">
                     <h4>Dungeons & Dragons</h4>
                 </div>
             <div class="book">
-            <img src="{{ URL::asset('assets/imgs/book.jpg') }}" alt="">
+                <img src="book.jpg" alt="">
                 <h4>Florida Man</h4>
             </div>
         </div>
@@ -150,7 +105,9 @@
                 <input type="radio" name="accordion" id="first" checked>
                     <div class="content">
                         <p>
-                            SevenBooks, platform online yang mempersembahkan akses mudah dan inspiratif ke beragam pengetahuan melalui koleksi buku terbaru dan komunitas pembaca yang dinamis
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Nulla incidunt qui quos officiis laudantium ipsa, fugiat
+                            quisquam obcaecati dolores porro?
                         </p>
                     </div>
             </li>
@@ -192,7 +149,7 @@
     <footer>
         <div class="left-frame">
            <div class="footer-brand">
-           <img src="{{ URL::asset('assets/imgs/logo.png') }}" alt="">
+                <img src="sevbooks-logo (1).png" alt="">
                 <h2>Seven Books</h2>
            </div>
            <p>Sail through the sea of knowledge</p>
