6853524b16195dd00f7f73f8da5296d328a87e95
Hkaar <HkaarDev@gmail.com>
2024-04-17 13:22:59 +0800

    Updated author view to use dashboard side bar component

--- resources/views/authors/index.blade.php
+++ resources/views/authors/index.blade.php
@@ -3,48 +3,7 @@
 @section('title', "Dashboard - Authors")
 
 @section('content')
-<nav id="side-nav" class="d-none d-md-block" data-collapsed="false">
-  <div class="nav-items">
-    <a href="#" class="nav-item side-nav-open">
-      <i class="fa-solid fa-arrow-right"></i>
-    </a>
-
-    <a href="#" class="nav-item side-nav-close">
-      <i class="fa-solid fa-arrow-left"></i>
-      <span class="nav-item-title">Close</span>
-    </a>
-
-    <a href="{{ route('users.index') }}" class="nav-item">
-      <i class="fa-solid fa-user"></i>
-      <span class="nav-item-title">Users</span>
-    </a>
-
-    <a href="{{ route('orders.index') }}" class="nav-item">
-      <i class="fa-solid fa-list"></i>
-      <span class="nav-item-title">Orders</span>
-    </a>
-
-    <a href="{{ route('books.index') }}" class="nav-item">
-      <i class="fa-solid fa-book"></i>
-      <span class="nav-item-title">Books</span>
-    </a>
-
-    <a href="#" class="nav-item active">
-      <i class="fa-solid fa-pen"></i>
-      <span class="nav-item-title">Authors</span>
-    </a>
-
-    <a href="{{ route('genres.index') }}" class="nav-item">
-      <i class="fa-solid fa-tag"></i>
-      <span class="nav-item-title">Genres</span>
-    </a>
-
-    <a href="#" class="nav-item mt-auto">
-      <i class="fa-solid fa-gear"></i>
-      <span class="nav-item-title">Settings</span>
-    </a>
-  </div>
-</nav>
+<x-dashboard-side-bar selected="author"></x-dashboard-side-bar>
 
 <div id="dashboardLeftFrame" class="container">
   <div class="action-bar mb-3">
