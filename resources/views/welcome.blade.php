ad42abc548fe3165eaf05e0e1b0c187c9a7b832d
berayenzz <tuturihandayani11577@gmail.com>
2024-04-17 21:35:30 +0800

    aku benerin sesuatu

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,7 +27,7 @@
             <a class="nav-link active cursor-pointer" aria-current="page" href="#">Browse</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link cursor-pointer" href="#">About us</a>
+            <a class="nav-link" href="#">About us</a>
           </li>
           </li>
           <li class="nav-item">
@@ -48,29 +48,128 @@
             <h5>Looking for a way to read a book from a library? Use our </h5><h5>service to read books from your local library</h5>   
         </div>
         <div class="d-flex">
-            <button class="btn btn-secondary me-3">What is this?</button>
-            <button class="btn btn-primary">Get Started </button>
+            <button class="btn btn-secondary me-3 pt-2 pb-2">What is this?</button>
+            <button class="btn btn-primary pt-2 pb-2">Get Started </button>
         </div>
     </div>
     <div class="hero-brand">
         <img src="{{ URL::asset('assets/imgs/7books.png') }}" alt="">
     </div>
 </section>
-<section class="about">
+<section class="about pt-5">
     <div class="message">
         <h2>Who we are</h2>
-        <p class="mb-1">We are a bunch of nerdy bookworm students that</p>
-        <p class="mb-1">provide a easy way to borrow books from a library</p>
-        <p class="mb-1">through an online platform</p>
+        <p class="mb-1">We are a bunch of nerdy bookworm students that <br>
+        provide a easy way to borrow books from a library <br>
+        through an online platform
+        </p>
     </div>
     <div class="message">
-        <h2>Our Mission</h2>
-        <p class="mb-1">Our mission is to provide an easy way for anyone to </p>
-        <p class="mb-1">borrow books from their local library, and provide a </p>
-        <p class="mb-1">way to read books from places that people forget </p>
-        <p class="mb-1">nowadays</p>
+        <h2 class="pt-3">Our Mission</h2>
+        <p class="mb-1">Our mission is to provide an easy way for anyone to <br>
+        borrow books from their local library, and provide a <br> way to read books
+        from places that people forget <br> nowadays
+        </p>
     </div>
 </section>
+<section class="books-showcase d-block justify-content-center align-items-center mb-5">
+    <h3 class="text-left mt-5">Curently Trending</h3>
+        <div class="books-slide">
+            <div class="book">
+                <img src="{{ URL::asset('assets/imgs/book.jpg') }}" alt="">
+                <h5 class="text-center mt-2">Harry Potter</h5>
+            </div>
+                <div class="book">
+                    <img src="{{ URL::asset('assets/imgs/book.jpg') }}" alt="">
+                    <h5 class="text-center mt-2">Lord of the Rings</h5>
+                </div>
+                <div class="book">
+                    <img src="{{ URL::asset('assets/imgs/book.jpg') }}" alt="">
+                    <h5 class="text-center mt-2">Dungeons & Dragons</h5>
+                </div>
+            <div class="book">
+                <img src="{{ URL::asset('assets/imgs/book.jpg') }}" alt="">
+                <h5 class="text-center mt-2">Florida Man</h5>
+            </div>
+        </div>
+        <h3 class="text-left mt-5">Newly Released</h3>
+        <div class="books-slide">
+            <div class="book">
+                <img src="{{ URL::asset('assets/imgs/book.jpg') }}" alt="">
+                <h5 class="text-center mt-2">Harry Potter</h5>
+            </div>
+                <div class="book">
+                    <img src="{{ URL::asset('assets/imgs/book.jpg') }}" alt="">
+                    <h5 class="text-center mt-2">Lord of the Rings</h5>
+                </div>
+                <div class="book">
+                    <img src="{{ URL::asset('assets/imgs/book.jpg') }}" alt="">
+                    <h5 class="text-center mt-2">Dungeons & Dragons</h5>
+                </div>
+            <div class="book">
+                <img src="{{ URL::asset('assets/imgs/book.jpg') }}" alt="">
+                <h5 class="text-center mt-2">Florida Man</h5>
+            </div>
+        </div>
+</section>
+<section class="faq d-flex justify-content-around align-items-center">
+    <div class="message">
+        <h4>FAQ</h4>
+        <p>Want to know more? See the answers to our <br>
+        commonly asked questions
+        </p>
+    </div>
+    <div class="accordion" id="accordionPanelsStayOpenExample">
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                    What is Seven Books?
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+                <div class="accordion-body">
+                    <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                    How do i do search a book?
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse">
+                <div class="accordion-body">
+                    <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                    How do i pickup a book
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse">
+                <div class="accordion-body">
+                    <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                    How do i rate a book
+                </button>
+            </h2>
+        <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse">
+            <div class="accordion-body">
+                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+        </div>
+    </div>
+</section>
+
   <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   <script defer src="{{ URL::asset('js/app.js'); }}"></script>
 </body>
