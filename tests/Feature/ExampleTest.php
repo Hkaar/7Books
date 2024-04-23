292c23d47a3693208cc82937347d25a62e2e5ea7
Hkaar <HkaarDev@gmail.com>
2024-04-23 08:17:04 +0800

    Added a test case

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -5,6 +5,8 @@ namespace Tests\Feature;
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
+use App\Models\User;
+
 class ExampleTest extends TestCase
 {
     /**
@@ -13,6 +15,19 @@ class ExampleTest extends TestCase
     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Tests whether the rating a book route works
+     */
+    public function test_book_rate(): void 
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $response = $this->post('/books/8/rate?rating=4');
 
         $response->assertStatus(200);
     }
