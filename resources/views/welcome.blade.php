849e836ce96cbea8cdcdee3cef4d1d7059168af5
berayenzz <tuturihandayani11577@gmail.com>
2024-05-23 23:05:44 +0800

    update responsive

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,7 +20,7 @@
           <a class="nav-link active cursor-pointer" aria-current="page" href="#">Browse</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">About us</a>
+          <a class="nav-link" href="#about">About us</a>
         </li>
         </li>
         <li class="nav-item">
@@ -36,16 +36,25 @@
 </nav>
 
 <section id="hero">
-  <div class="container mb-5">
-    <div class="row">
+  <div class="container">
+    <div class="row justify-content-center align-items-center mb-5">
       <div class="col-md-5 d-block d-md-none">
         <img src="{{ URL::asset('assets/imgs/7books.png') }}" class="img-fluid">
       </div>
-      <div class="col-md-7 mt-5">
-        <h1>Sail The Knowledge Seas</h1>
-        <h6 class="text-secondary lh-lg">Looking for a way to read a book from a library? Use our <br>
-         service to read books from your local library</h6>
-         <div class="flex mt-3">
+      <div class="col-md-6 d-block d-md-none text-center">
+        <h4 class="fw-bold">Sail The Knowledge Seas</h4>
+        <p class="text-secondary lh-lg ">Looking for a way to read a book from a library? Use our
+         service to read books from your local library</p>
+         <div class="flex">
+          <button class="btn btn-secondary fw-medium">What is this</button>
+          <button class="btn btn-primary ms-3 fw-medium">Get Started</button>
+         </div>
+      </div>
+      <div class="col-md-6 d-none d-md-block">
+        <h2 class="fw-bold">Sail The Knowledge Seas</h2>
+        <p class="text-secondary lh-lg">Looking for a way to read a book from a library? Use our 
+         service to read books from your local library</p>
+         <div class="flex mt-2">
           <button class="btn btn-secondary fw-medium">What is this</button>
           <button class="btn btn-primary ms-3 fw-medium">Get Started</button>
          </div>
@@ -56,97 +65,103 @@
     </div>
   </div>
 </section>
-<section id="about">
+<section id="about" style="padding-top:3rem;">
   <div class="container">
-    <div class="row justify-content-around">
-      <div class="col-md-5">
-        <h3>Who We Are</h3>
-        <p>We  are a bunch of nerdy bookworm students that provide 
-          a easy way to borrow books from a library through an online platform</p>
+    <div class="row justify-content-evenly align-items-center">
+      <div class="col-md-6 d-block d-md-none">
+        <h4 class="text-center">Who We Are</h4>
+        <p>We are a bunch of nerdy bookworm students that provide 
+            a easy way to borrow books from a library through an online platform</p>
       </div>
-      <div class="col-md-5">
+      <div class="col-md-6 d-block d-md-none mt-4">
+        <h4 class="text-center">Our Mission</h4>
+        <p>Our mission is to provide an easy way for anyone to borrow books from their local library,
+            and provide a way to read books from places that people forget nowadays</p>
+      </div>
+      <div class="col-md-5 d-none d-md-block">
+        <h3>Who We Are</h3>
+        <p>We are a bunch of nerdy bookworm students that provide 
+            a easy way to borrow books from a library through an online platform</p>
+      </div>
+      <div class="col-md-5 d-none d-md-block">
         <h3>Our Mission</h3>
         <p>Our mission is to provide an easy way for anyone to borrow books from their local library,
-           and provide a way to read books from places that people forget nowadays</p>
+            and provide away to read books from places that people forget nowadays</p>
       </div>
     </div>
   </div>
 </section>
-<section id="books">
+
+<section id="books" style="padding-top:6rem;">
   <div class="container">
-    <div class="row">
-      <div class="col-md-6">
-      <h4 class="fw-normal mb-3">Currently Trending</h4>
-      </div>
-    </div>
-    <div class="row text-center">
+    <h4 class="fw-normal mb-3 d-none d-md-block">Currently Trending</h4>
+    <h5 class="fw-normal mb-3 d-block d-md-none text-center">Currently Trending</h5>
+    <div class="row justify-content-center align-items-center">
       <div class="col-md-3">
-        <div class="border-0">
-          <img src="{{ URL::asset('assets/imgs/book.jpg') }}" class="img-fluid" style="width:245px; height:377px">
-          <div class="card-body mt-3">
+        <div class="card border-0">
+          <img src="{{ URL::asset('assets/imgs/book.jpg') }}" class="img-fluid">
+          <div class="card-body">
             <p class="fw-semibold">Harry Potter</p>
           </div>
         </div>
       </div>
       <div class="col-md-3">
-        <div class="border-0">
-          <img src="{{ URL::asset('assets/imgs/book.jpg') }}" class="img-fluid" style="width:245px; height:377px">
-          <div class="card-body mt-3">
+        <div class="card border-0">
+          <img src="{{ URL::asset('assets/imgs/book.jpg') }}" class="img-fluid">
+          <div class="card-body">
             <p class="fw-semibold">Lord Of The Rings</p>
           </div>
         </div>
       </div>
       <div class="col-md-3">
-        <div class="border-0">
-          <img src="{{ URL::asset('assets/imgs/book.jpg') }}" class="img-fluid" style="width:245px; height:377px">
-          <div class="card-body mt-3">
+        <div class="card border-0">
+          <img src="{{ URL::asset('assets/imgs/book.jpg') }}" class="img-fluid">
+          <div class="card-body">
             <p class="fw-semibold">Dungeons & Dragons</p>
           </div>
         </div>
       </div>
       <div class="col-md-3">
-        <div class="border-0">
-          <img src="{{ URL::asset('assets/imgs/book.jpg') }}" class="img-fluid" style="width:245px; height:377px">
-          <div class="card-body mt-3">
+        <div class="card border-0">
+          <img src="{{ URL::asset('assets/imgs/book.jpg') }}" class="img-fluid">
+          <div class="card-body">
             <p class="fw-semibold">Florida Man</p>
           </div>
         </div>
       </div>
     </div>
-    <div class="row">
-      <div class="col-md-6">
-      <h4 class="fw-normal mb-3 mt-5">Newly Released</h4>
-      </div>
-    </div>
-    <div class="row text-center">
+    
+    <h5 class="fw-normal mb-3 mt-5 d-block d-md-none text-center">Newly Released</h5>
+    <h4 class="fw-normal mb-3 mt-5 d-none d-md-block">Newly Released</h4>
+    <div class="row justify-content-center align-items-center">
       <div class="col-md-3">
-        <div class="border-0">
-          <img src="{{ URL::asset('assets/imgs/book.jpg') }}" class="img-fluid" style="width:245px; height:377px">
-          <div class="card-body mt-3">
+        <div class="card border-0">
+          <img src="{{ URL::asset('assets/imgs/book.jpg') }}" class="img-fluid">
+          <div class="card-body">
             <p class="fw-semibold">Harry Potter</p>
           </div>
         </div>
       </div>
       <div class="col-md-3">
-        <div class="border-0">
-          <img src="{{ URL::asset('assets/imgs/book.jpg') }}" class="img-fluid" style="width:245px; height:377px">
-          <div class="card-body mt-3">
+        <div class="card border-0">
+          <img src="{{ URL::asset('assets/imgs/book.jpg') }}" class="img-fluid">
+          <div class="card-body">
             <p class="fw-semibold">Lord Of The Rings</p>
           </div>
         </div>
       </div>
       <div class="col-md-3">
-        <div class="border-0">
-          <img src="{{ URL::asset('assets/imgs/book.jpg') }}" class="img-fluid" style="width:245px; height:377px">
-          <div class="card-body mt-3">
+        <div class="card border-0">
+          <img src="{{ URL::asset('assets/imgs/book.jpg') }}" class="img-fluid">
+          <div class="card-body">
             <p class="fw-semibold">Dungeons & Dragons</p>
           </div>
         </div>
       </div>
       <div class="col-md-3">
-        <div class="border-0">
-          <img src="{{ URL::asset('assets/imgs/book.jpg') }}" class="img-fluid" style="width:245px; height:377px">
-          <div class="card-body mt-3">
+        <div class="card border-0">
+          <img src="{{ URL::asset('assets/imgs/book.jpg') }}" class="img-fluid">
+          <div class="card-body">
             <p class="fw-semibold">Florida Man</p>
           </div>
         </div>
@@ -154,16 +169,18 @@
     </div>
   </div>
 </section>
-<section id="faq">
+<section id="faq" style="padding-top:3rem;">
   <div class="container mt-5">
     <div class="row justify-content-around align-items-center">
-      <div class="col-md-6">
+      <div class="col-md-6 d-block d-md-none text-center">
         <h4>FAQ</h4>
-        <p>
-        Want to know more? See the answers to our <br> commonly asked questions
-        </p>
+        <p>Want to know more? See the answers to our commonly asked questions</p>
       </div>
-      <div class="col-md-6">
+      <div class="col-md-6 d-none d-md-block">
+        <h4>FAQ</h4>
+        <p>Want to know more? See the answers to our <br>commonly asked questions</p>
+      </div>
+      <div class="col-md-6 border-0">
       <div class="accordion" id="accordionPanelsStayOpenExample">
         <div class="accordion-item mb-3">
           <h2 class="accordion-header" id="panelsStayOpen-headingOne">
