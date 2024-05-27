6308bfbd4c65f33e9fd1f18673f19c12e4210627
Hkaar <HkaarDev@gmail.com>
2024-05-27 21:29:26 +0800

    Updated author migrations

--- database/migrations/2024_04_01_134019_authors.php
+++ database/migrations/2024_04_01_134019_authors.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create("authors", function(Blueprint $table) {
             $table->id();
-            $table->string("name");
+            $table->string("name")->unique();
             $table->string("address");
             $table->string("phone");
             $table->string("img")->nullable();
