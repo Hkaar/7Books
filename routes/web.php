da0cbbb3467a56e4615d28c89d0fc3b9a75b84f2
Hkaar <HkaarDev@gmail.com>
2024-05-28 21:17:06 +0800

    Updated web routes

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\OrdersController;
-use App\Http\Controllers\AuthorsController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\BooksController;
-use App\Http\Controllers\GenreController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -19,11 +13,18 @@ use App\Http\Controllers\GenreController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name("/");
-
 Route::group(["namespace" => "App\Http\Controllers"], function() {
+    Route::get("/", "HomeController@home")->name("/");
+    Route::get("/browse", "HomeController@browse")->name("/browse");
+    Route::get("/denied", "HomeController@denied")->name("denied");
+
+    Route::group(["middleware" => ["auth"]], function() {
+        Route::get("/logout", "LogoutController@perform")->name("logout");
+        Route::get("/me", "HomeController@userShow")->name("user.me");
+
+        Route::post("/books/{id}/rate", "BooksController@rate")->name("books.rate");
+    });
+
     Route::group(["middleware" => ["guest"]], function() {
         Route::get("/register", "RegisterController@show")->name("register.show");
         Route::post("/register", "RegisterController@register")->name("register");
@@ -32,35 +33,22 @@ Route::group(["namespace" => "App\Http\Controllers"], function() {
         Route::post("/login", "LoginController@login")->name("login");
     });
 
-    Route::group(["middleware" => ["auth"]], function() {
-        Route::get("/logout", "LogoutController@perform")->name("logout");
-    });
-});
-
-Route::get("/denied", function() {
-    return view("auth.denied");
-})->name("denied");
-
-Route::middleware("auth")->group(function() {
-    Route::post("/books/{id}/rate", [BooksController::class, "rate"])->name("books.rate");
-});
-
-Route::prefix("/manage")->middleware(["auth", "check.level"])->group(function() {
-    Route::get("/books/select", [BooksController::class, "select"])->name("books.select");
-    Route::get("/books/multi-select", [BooksController::class, "multiSelect"])->name("books.multi-select");
-    
-    Route::get("/authors/authored/{id}", [AuthorsController::class, "authored"])->name("authors.authored");
-    Route::get("/orders/items/{id}", [OrdersController::class, "items"])->name("orders.items");
-    
-    Route::resource("/books", BooksController::class)->names("books");
-    Route::resource("/orders", OrdersController::class)->names("orders");
-    Route::resource("/authors", AuthorsController::class)->names("authors");
-    Route::resource("/genres", GenreController::class)->names("genres");
-    
-    Route::middleware("check.admin")->group(function() {
-        Route::resource("/users", UserController::class)->names("users");
+    Route::group(["prefix" => "/manage", "middleware" => ["auth", "check.level"]], function() {
+        Route::get("/books/select", "BooksController@select")->name("books.select");
+        Route::get("/books/multi-select", "BooksController@multiSelect")->name("books.multi-select");
+        
+        Route::get("/authors/authored/{id}", "AuthorsController@authored")->name("authors.authored");
+        Route::get("/orders/items/{id}", "OrdersController@items")->name("orders.items");
+        
+        Route::resource("/books", "BooksController")->names("books");
+        Route::resource("/orders", "OrdersController")->names("orders");
+        Route::resource("/authors", "AuthorsController")->names("authors");
+        Route::resource("/genres", "GenreController")->names("genres");
+        
+        Route::middleware("check.admin")->group(function() {
+            Route::resource("/users", "UserController")->names("users");
+        });
     });
 });
 
 Route::redirect("/manage", "/manage/orders");
-Route::get("/browse", [BooksController::class, "browse"])->name("browse");
