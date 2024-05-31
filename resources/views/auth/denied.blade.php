0816f5c8483656e6550f55c76b7dea57b15ccfe3
Hkaar <HkaarDev@gmail.com>
2024-05-31 21:09:39 +0800

    Updated denied view

--- resources/views/auth/denied.blade.php
+++ resources/views/auth/denied.blade.php
@@ -4,25 +4,12 @@
 
 @section('main')
 <div class="container min-vh-100 d-flex flex-column align-items-center justify-content-center">
-  <div class="shadow p-4 rounded">
-    <a href="{{ route('/') }}" class="d-flex align-items-center justify-content-center flex-column flex-md-row gap-2 mb-3 text-center text-inherit text-none">
-      <img src="{{ URL::asset('assets/imgs/logo.png') }}" alt="Image not available" class="img-fluid ratio-box img-logo">
-      <h3>Seven Books</h3>
-    </a>
-  
-    <div class="d-flex flex-column align-items-center justify-content-center">  
-      <div class="text-center">
-        <h3 class="text-danger mb-3">Sorry But You Can't Access That Page</h3>
+  <div class="d-flex flex-column align-items-center justify-content-center">  
+    <div class="text-center">
+      <h3 class="text-danger mb-3">Sorry but you can't access that yet</h3>
 
-        <p class="mb-3">
-          The page you requested cannot be accessed...
-        </p>
-  
-        <div class="d-flex gap-1 justify-content-center align-items-center w-100">
-          <a href="{{ route('orders.index') }}" class="btn btn-danger">Go back</a>
-          <button class="btn btn-info">What is this?</button>
-          <a href="{{ route('/') }}" class="btn btn-primary">Go to home</a>
-        </div>
+      <div class="d-flex gap-1 justify-content-center align-items-center w-100">
+        <a href="{{ route('/') }}" class="btn btn-primary">Go to home</a>
       </div>
     </div>
   </div>
