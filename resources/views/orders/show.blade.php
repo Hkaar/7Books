9a84cd1c87c01633e25d1d41b6ea5bd87f2a5ca2
Hkaar <HkaarDev@gmail.com>
2024-05-31 21:09:29 +0800

    Updated show view for orders

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -1,34 +1,34 @@
 <div class="row h-100">
   <div class="col-12 col-lg-6">
-    <article class="info mb-3">
+    <article class="d-flex flex-column gap-1 flex-fill mb-3">
       <b>User ID :</b>
       {{ $order->user_id }}
     </article>
   </div>
 
   <div class="col-12 col-lg-6">
-    <article class="info mb-3">
+    <article class="d-flex flex-column gap-1 flex-fill mb-3">
       <b>Token :</b>
       {{ $order->token }}
     </article>
   </div>
 
   <div class="col-12 col-lg-6">
-    <article class="info mb-3">
+    <article class="d-flex flex-column gap-1 flex-fill mb-3">
       <b>Return by :</b>
       {{ $order->return_date }}
     </article>
   </div>
 
   <div class="col-12 col-lg-6">
-    <article class="info mb-3">
+    <article class="d-flex flex-column gap-1 flex-fill mb-3">
       <b>Placed at :</b>
       {{ $order->placed }}
     </article>
   </divcol-12>
 
   <div class="col-12 col-lg-6">
-    <article class="info mb-3">
+    <article class="d-flex flex-column gap-1 flex-fill mb-3">
       <b>Status :</b>
       {{ $order->status }}
     </article>
