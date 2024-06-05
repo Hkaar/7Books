71b67f85834698dc813f7d43a484dc32ecb16990
Hkaar <HkaarDev@gmail.com>
2024-06-05 17:31:57 +0800

    Added home route & filter routes

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(["namespace" => "App\Http\Controllers"], function() {
-    Route::get("/", "HomeController@home")->name("/");
+    Route::get("/", "HomeController@welcome")->name("/");
     Route::get("/browse", "HomeController@browse")->name("/browse");
     Route::get("/denied", "HomeController@denied")->name("denied");
 
     Route::group(["middleware" => ["auth"]], function() {
         Route::get("/logout", "LogoutController@perform")->name("logout");
+
+        Route::get("/home", "HomeController@home")->name("home");
         Route::get("/me", "HomeController@userShow")->name("user.me");
 
-        Route::post("/books/{id}/rate", "BooksController@rate")->name("books.rate");
+        Route::post("/books/{id}/rate", "BookController@rate")->name("books.rate");
     });
 
     Route::group(["middleware" => ["guest"]], function() {
@@ -34,19 +36,24 @@ Route::group(["namespace" => "App\Http\Controllers"], function() {
     });
 
     Route::group(["prefix" => "/manage", "middleware" => ["auth", "check.level"]], function() {
-        Route::get("/books/select", "BooksController@select")->name("books.select");
-        Route::get("/books/multi-select", "BooksController@multiSelect")->name("books.multi-select");
+        Route::get("/books/select", "BookController@select")->name("books.select");
+        Route::get("/books/multi-select", "BookController@multiSelect")->name("books.multi-select");
         
-        Route::get("/authors/authored/{id}", "AuthorsController@authored")->name("authors.authored");
-        Route::get("/orders/items/{id}", "OrdersController@items")->name("orders.items");
+        Route::get("/authors/authored/{id}", "AuthorController@authored")->name("authors.authored");
+        Route::get("/orders/items/{id}", "OrderController@items")->name("orders.items");
+
+        Route::get("/books/filter", "BookController@filter")->name("books.filter");
+        Route::get("/orders/filter", "OrderController@filter")->name("orders.filter");
+        Route::get("/authors/filter", "AuthorController@filter")->name("authors.filter");
+        Route::get("/genres/filter", "GenreController@filter")->name("genres.filter");
         
-        Route::resource("/books", "BooksController")->names("books");
-        Route::resource("/orders", "OrdersController")->names("orders");
-        Route::resource("/authors", "AuthorsController")->names("authors");
+        Route::resource("/books", "BookController")->names("books");
+        Route::resource("/orders", "OrderController")->names("orders");
+        Route::resource("/authors", "AuthorController")->names("authors");
         Route::resource("/genres", "GenreController")->names("genres");
         
         Route::middleware("check.admin")->group(function() {
-            Route::get("/users/filter", "UserController@applyFilter")->name("users.filter");
+            Route::get("/users/filter", "UserController@filter")->name("users.filter");
             Route::resource("/users", "UserController")->names("users");
         });
     });
