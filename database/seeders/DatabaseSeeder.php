8cce7ceb512d5e923743f36a25b253af56ceb4b2
Hkaar <HkaarDev@gmail.com>
2024-06-06 17:34:19 +0800

    Updated database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            UserSeeder::class,
+            OrderSeeder::class,
+            BookSeeder::class,
+            AuthorSeeder::class,
+            GenreSeeder::class,
+        ]);
     }
 }
