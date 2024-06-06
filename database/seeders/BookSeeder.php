e0e354f51b4eba36b06322f663a82257fc1676ed
Hkaar <HkaarDev@gmail.com>
2024-06-06 17:32:22 +0800

    Added Book Seeder

---
+++ database/seeders/BookSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\BookRating;
+use App\Models\Genre;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
