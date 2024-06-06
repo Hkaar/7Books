00898b55697a722e21031db370c303b5f1b16b83
Hkaar <HkaarDev@gmail.com>
2024-06-06 17:31:41 +0800

    Added User Seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\BookRating;
+use App\Models\Order;
+use App\Models\User;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
