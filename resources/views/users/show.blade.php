f5358a8a8880325427bffab2eca70895722b171a
Hkaar <HkaarDev@gmail.com>
2024-06-13 22:59:15 +0800

    Removed extra spacing in users show

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -14,7 +14,7 @@
 
       <article class="d-flex flex-column gap-1 flex-fill">
         <b>Username</b>
-        {{ $user->username }}
+        {{ $user->username }}    
       </article>
       
       <article class="d-flex flex-column gap-1 flex-fill">
