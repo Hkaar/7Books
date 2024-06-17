5d7bd9cae4faa1db0534d017751b3ac8e6753d5a
Hkaar <HkaarDev@gmail.com>
2024-06-17 21:14:43 +0800

    Updated paginate links component

--- resources/views/components/paginate-links.blade.php
+++ resources/views/components/paginate-links.blade.php
@@ -1,28 +1,67 @@
-<div {{ $attributes->merge(["class" => "overflow-auto"]) }}>
+<div {{ $attributes->merge(["class" => "overflow-auto py-3"]) }}>
   <ul class="pagination" style="margin: 0">
   @if ($links->onFirstPage())
     <li class="page-item disabled">
-      <span class="page-link">Previous</span>
+      <span class="page-link d-flex align-items-center gap-2">
+        <i class="fa-solid fa-chevron-left"></i>
+        Previous
+      </span>
     </li>
   @else
     <li class="page-item">
-      <a class="page-link" href="{{ $links->previousPageUrl() }}">Previous</a>
+      @if ($useHtmx)
+        <a class="page-link d-flex align-items-center gap-2" href="" hx-get="{{ $links->previousPageUrl() }}">
+          <i class="fa-solid fa-chevron-left"></i>
+          Previous
+        </a>
+      @else
+        <a class="page-link d-flex align-items-center gap-2" href="{{ $links->previousPageUrl() }}">
+          <i class="fa-solid fa-chevron-left"></i>
+          Previous
+        </a>
+      @endif
     </li>
   @endif
    
   @for ($i = 1; $i <= $links->lastPage(); $i++)
-    <li class="page-item {{ ($links->currentPage() == $i) ? 'active' : '' }}">
-      <a class="page-link" href="{{ $links->url($i) }}">{{ $i }}</a>
-    </li>
+    @if (($i >= $links->currentPage()-1 && $i <= $links->currentPage()+2) || $i == 1 || $i == $links->lastPage())
+      @if ($useHtmx)
+        <li class="page-item {{ ($links->currentPage() == $i) ? 'active' : '' }}">
+          <a class="page-link" href="" hx-get="{{ $links->url($i) }}">
+            {{ $i }}
+          </a>
+        </li>
+      @else
+        <li class="page-item {{ ($links->currentPage() == $i) ? 'active' : '' }}">
+          <a class="page-link" href="{{ $links->url($i) }}">
+            {{ $i }}
+          </a>
+        </li>
+      @endif
+    @endif
   @endfor
 
   @if ($links->hasMorePages())
     <li class="page-item">
-      <a class="page-link" href="{{ $links->nextPageUrl() }}">Next</a>
+      @if ($useHtmx)
+        <a class="page-link d-flex align-items-center gap-2" href="" hx-get="{{ $links->nextPageUrl() }}">
+          Next
+          <i class="fa-solid fa-chevron-right"></i>
+        </a>
+      @else
+        <a class="page-link d-flex align-items-center gap-2" href="{{ $links->nextPageUrl() }}">
+          Next
+          <i class="fa-solid fa-chevron-right"></i>
+        </a>
+      @endif
+      
     </li>
   @else
     <li class="page-item disabled">
-      <span class="page-link">Next</span>
+      <span class="page-link d-flex align-items-center gap-2">
+        Next
+        <i class="fa-solid fa-chevron-right"></i>
+      </span>
     </li>
   @endif
 </ul>
