a252f347ca80f83fe052a032f91a455655563158
Hkaar <HkaarDev@gmail.com>
2024-06-20 21:37:43 +0800

    Updated filter service to add filter method

--- app/Services/FilterService.php
+++ app/Services/FilterService.php
@@ -6,4 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class FilterService
 {
+    /**
+     * Applies a set of filters for fetching items of a model
+     */
+    public function filter(Model $model, array $scopes, array $filters)
+    {
+        $result = $model->query();
+
+        foreach ($scopes as $scope => $params) 
+        {
+            if (method_exists($model, $scope)) {
+                $result->{$scope}(...$params);
+            }
+        }
+
+        foreach ($filters as $filter) 
+        {
+            if ($filter === "latest") {
+                $result->latest();
+            } elseif ($filter === "oldest") {
+                $result->oldest();
+            }
+        }
+
+        return $result->paginate(20);
+    }
 }
