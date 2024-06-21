db0e19b069d8e7cf59f1edf3eec02ee428e186c3
Hkaar <HkaarDev@gmail.com>
2024-06-21 11:30:59 +0800

    Moved model updater trait from App/Http/Traits to App/Traits

---
+++ app/Traits/ModelUpdater.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Database\Eloquent\Model;
+
+trait ModelUpdater 
+{
+    /**
+     * Updates the model based on if the data exists
+     */
+    public function updateModel(Model $model, array $data, array $except = []) {
+        $attributes = array_keys($model->getAttributes());
+
+        foreach ($data as $key => $value) {
+            if (!in_array($key, $except) && in_array($key, $attributes) && $model->$key !== $value) {
+                $model->$key = $value ?? $model->$key;
+            }
+        }
+    }
+}
