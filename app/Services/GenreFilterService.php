d7b6af6f428915ea3ccd78030b747d61d3fab997
Hkaar <HkaarDev@gmail.com>
2024-06-21 11:32:58 +0800

    Added genre filter service

---
+++ app/Services/GenreFilterService.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Genre;
+use App\Traits\GenericFilter;
+
+class GenreFilterService
+{
+    use GenericFilter;
+
+    /**
+     * Applies filters to query the book model
+     */
+    public function filter(array $filters)
+    {
+        $genres = Genre::query();
+        $filterKeys = array_keys($filters);
+
+        $this->genericFilter($genres, $filters);
+
+        if (in_array("search", $filterKeys) && !empty($filters["search"])) {
+            $genres->where("name", "like", "%".$filters["search"]."%");
+        }
+
+        return $genres->paginate(20);
+    }
+}
