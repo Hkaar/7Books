fa6149bb1f726ee7b6c9bc742c8c0b75208b853e
Hkaar <HkaarDev@gmail.com>
2024-06-27 14:29:38 +0800

    Added service test in feature tests

---
+++ tests/Feature/ServiceTest.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use App\Services\UserFilterService;
+use App\Services\OrderFilterService;
+use App\Services\BookFilterService;
+use App\Services\AuthorFilterService;
+use App\Services\GenreFilterService;
+
+class ServiceTest extends TestCase
+{
+    /**
+     * Test whether the user filter service is working
+     */
+    public function test_user_filter_service(): void
+    {
+        $filters = [
+            "search" => "m",
+            "level" => "member",
+            "latest",
+        ];
+
+        $service = new UserFilterService();
+        $results = $service->filter($filters);
+
+        $this->assertNotEmpty($results);
+    }
+
+    /**
+     * Test whether the order filter service is working
+     */
+    public function test_order_filter_service(): void
+    {
+        $filters = [
+            "search" => "m",
+            "latest",
+        ];
+
+        $service = new OrderFilterService();
+        $results = $service->filter($filters);
+
+        $this->assertNotEmpty($results);
+    }
+
+    /**
+     * Test whether the book filter service is working
+     */
+    public function test_book_filter_service(): void
+    {
+        $filters = [
+            "search" => "m",
+            "latest",
+        ];
+
+        $service = new BookFilterService();
+        $results = $service->filter($filters);
+
+        $this->assertNotEmpty($results);
+    }
+
+    /**
+     * Test whether the author filter service is working
+     */
+    public function test_author_filter_service(): void
+    {
+        $filters = [
+            "search" => "m",
+            "latest",
+        ];
+
+        $service = new AuthorFilterService();
+        $results = $service->filter($filters);
+
+        $this->assertNotEmpty($results);
+    }
+
+    /**
+     * Test whether the genre filter service is working
+     */
+    public function test_genre_filter_service(): void
+    {
+        $filters = [
+            "search" => "m",
+            "latest",
+        ];
+
+        $service = new GenreFilterService();
+        $results = $service->filter($filters);
+
+        $this->assertNotEmpty($results);
+    }
+}
