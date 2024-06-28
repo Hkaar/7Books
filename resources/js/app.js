534a94758a24f8854077ab41ec5846285599307d
Hkaar <HkaarDev@gmail.com>
2024-06-28 17:31:46 +0800

    Added swipers for home in js

--- resources/js/app.js
+++ resources/js/app.js
@@ -171,10 +171,39 @@ document.addEventListener("DOMContentLoaded", () => {
         addItem(this);
     });
 
-    let swiper = new Swiper(".mySwiper", {
+    let homeHeroSwiper = new Swiper("#homeHeroSwiper", {
         effect: "cards",
         grabCursor: true,
-      });
+        centeredSlides: true,
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            type: "fraction",
+        },
+    });
+
+    let swiper = new Swiper(".bookSwiper", {
+        slidesPerView: 4.5,
+        spaceBetween: 20,
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+
+        navigation: { 
+            nextEl: '.book-swiper-button-next', 
+            prevEl: '.book-swiper-button-prev', 
+        }, 
+    });
 
     $(document).on("htmx:confirm", showConfirm);
 
