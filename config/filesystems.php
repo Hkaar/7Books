dd88b1b4ce35fdf43f5dd006ed4c3a46adc1829f
Hkaar <HkaarDev@gmail.com>
2024-06-28 23:16:35 +0800

    Added backup disk

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,6 +36,12 @@ return [
             'throw' => false,
         ],
 
+        'backup' => [
+            'driver' => 'local',
+            'root' => storage_path('app/backup'),
+            'throw' => false,
+        ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
