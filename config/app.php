d15e9c2378e9f2dfe397323073cb940fa31675c1
Hkaar <HkaarDev@gmail.com>
2024-06-28 23:45:03 +0800

    Added Backup Service Provider

--- config/app.php
+++ config/app.php
@@ -171,6 +171,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
+        Spatie\Backup\BackupServiceProvider::class,
     ])->toArray(),
 
     /*
