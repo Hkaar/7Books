2c11952dd0d32eee8e51aca86cba3c9355d088c1
Hkaar <HkaarDev@gmail.com>
2024-07-01 11:10:40 +0800

    Added model for library

---
+++ app/Models/Library.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Library extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'libraries';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'desc',
+        'region_id',
+    ];
+
+    /**
+     * Define the relationship with regions
+     */
+    public function region()
+    {
+        return $this->belongsTo(Region::class, "region_id", "id");
+    }
+
+    /**
+     * Define the relationship with books
+     */
+    public function books()
+    {
+        return $this->belongsToMany(Book::class, "library_books");
+    }
+}
