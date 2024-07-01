be9289380af90c64b7ed1adbe71dfceb7182d8e1
Hkaar <HkaarDev@gmail.com>
2024-07-01 20:58:15 +0800

    Attempted fix again

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\View\Components\SVBNavigationBar;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blade::component(SVBNavigationBar::class, "svb-navigation-bar");
     }
 }
