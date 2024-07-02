4ab6a23914afb7fb8d9a2176a74027ad0db52120
Hkaar <HkaarDev@gmail.com>
2024-07-02 20:01:40 +0800

    Added user regions migration

---
+++ database/migrations/2024_07_02_084543_user_regions.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("user_regions", function(Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id")->constrained("users");
+            $table->foreignId("region_id")->constrained("regions");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists("user_regions");
+    }
+};
