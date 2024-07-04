79628308a7fe7ca408434a7ddc2c03bcb439aa92
Hkaar <HkaarDev@gmail.com>
2024-07-04 13:54:00 +0800

    Added role seeder

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Role::create([
+            "name" => "admin",
+        ]);
+
+        Role::create([
+            "name" => "operator",
+        ]);
+
+        Role::create([
+            "name" => "member",
+        ]);
+    }
+}
