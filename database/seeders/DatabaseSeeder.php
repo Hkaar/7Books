5daaed0bdc8c34e2ff6d1c91d909f4d9dca115fd
Hkaar <HkaarDev@gmail.com>
2024-07-04 13:54:45 +0800

    Updated database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
-            BookSeeder::class,
+            RoleSeeder::class,
+            StatusSeeder::class,
         ]);
     }
 }
