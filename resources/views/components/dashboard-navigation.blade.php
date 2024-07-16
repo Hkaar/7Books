17e17a315c414786b71f9b4b852c2f1497753255
Hkaar <HkaarDev@gmail.com>
2024-07-16 21:59:33 +0800

    Updated dashboard nav bar

--- resources/views/components/dashboard-navigation.blade.php
+++ resources/views/components/dashboard-navigation.blade.php
@@ -1,14 +1,15 @@
-<nav {{ $attributes->merge(["class" => "navbar navbar-expand-md shadow-sm py-2"]) }}>
+<nav {{ $attributes->merge(["class" => "navbar navbar-expand-md shadow-sm py-3"]) }}>
   <div class="container-fluid d-flex justify-content-between align-items-center">
-    <div class="d-flex gap-2">
+    <div class="d-flex gap-3">
       <button type="button" class="btn btn-light border side-nav-toggle">
         <i class="fa-solid fa-bars fs-5 m-0"></i>
       </button>
 
       <article class="d-flex gap-1 align-items-center">
         <img src="{{ URL::asset('assets/imgs/logo.png') }}" alt="Image not available" class="img-fluid ratio-box img-small-logo">
-        <span class="d-none d-md-block text-body-secondary">
-          {{ str_replace("manage", "Dashboard", request()->path()) }}
+
+        <span class="d-none d-md-flex align-items-center">
+          <x-svb-bread-crumb></x-svb-bread-crumb>
         </span>
 
         <span class="d-md-none text-body-secondary">
@@ -29,10 +30,6 @@
 
         <li class="nav-item">
           <a class="nav-link"  href="{{ route('browse') }}">Browse</a>
-        </li>
-
-        <li class="nav-item">
-          <a class="nav-link text-danger" href="{{ route('logout') }}">Logout</a>
         </li>
       </ul>
     </div>
