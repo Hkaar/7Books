50f843ff67978762129bf26c33a56f71d170b53a
Hkaar <HkaarDev@gmail.com>
2024-07-16 21:53:29 +0800

    Added region book model

---
+++ app/Models/RegionBook.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class RegionBook extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable
+     * 
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        "region_id",
+        "book_id",
+        "stock",
+    ];
+
+    /**
+     * Define relationship with regions
+     */
+    public function region()
+    {
+        return $this->belongsTo(Region::class, "region_id", "id");
+    }
+
+    /**
+     * Define relationship with books
+     */
+    public function book()
+    {
+        return $this->belongsTo(Book::class, "book_id", "id");
+    }
+}
