032679b7b31f3d72e01238701bab27a16b8b08e1
Hkaar <HkaarDev@gmail.com>
2024-07-16 21:59:02 +0800

    Updated library controller

--- app/Http/Controllers/LibraryController.php
+++ app/Http/Controllers/LibraryController.php
@@ -12,7 +12,11 @@ class LibraryController extends Controller
      */
     public function index()
     {
-        //
+        $libraries = Library::paginate(20);
+
+        return view("library.index", [
+            "libraries" => $libraries,
+        ]);
     }
 
     /**
@@ -20,7 +24,7 @@ class LibraryController extends Controller
      */
     public function create()
     {
-        //
+        return view("library.create");
     }
 
     /**
@@ -28,38 +32,67 @@ class LibraryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            "name" => "required|string|max:255",
+            "desc" => "required|string|max:255",
+            "region_id" => "required|numeric|exists:regions,id",
+        ]);
+
+        $library = new Library();
+        $library->fill($validated)->save();
+
+        return redirect()->route("library.index");
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Library $library)
+    public function show(int $id)
     {
-        //
+        $library = Library::findOrFail($id);
+
+        return view("library.show", [
+            "library" => $library,
+        ]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Library $library)
+    public function edit(int $id)
     {
-        //
+        $library = Library::findOrFail($id);
+
+        return view("library.edit", [
+            "library" => $library,
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Library $library)
+    public function update(Request $request, int $id)
     {
-        //
+        $validated = $request->validate([
+            "name" => "nullable|string|max:255",
+            "desc" => "nullable|string|max:255",
+            "region_id" => "nullable|numeric|exists:regions,id",
+        ]);
+
+        $library = new Library();
+        $library->fill($validated)->save();
+
+        return redirect()->route("library.index");
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Library $library)
+    public function destroy(int $id)
     {
-        //
+        $library = Library::findOrFail($id);
+
+        $library->books()->detach();
+        $library->delete();
     }
 }
