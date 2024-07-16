e5527aed4862112c2278e8fb8cea39452969364e
Hkaar <HkaarDev@gmail.com>
2024-07-16 21:53:17 +0800

    Added library book model

---
+++ app/Models/LibraryBook.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LibraryBook extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable
+     * 
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        "library_id",
+        "book_id",
+        "stock",
+    ];
+
+    /**
+     * Define the relationship with libraries
+     */
+    public function library()
+    {
+        return $this->belongsTo(Library::class, "library_id", "id");
+    }
+
+    /**
+     * Define relationship with books
+     */
+    public function book()
+    {
+        return $this->belongsTo(Book::class, "book_id", "id");
+    }
+}
