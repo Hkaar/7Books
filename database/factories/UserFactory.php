238671618589ab3d6a2643ba669d307d5dfd9436
Hkaar <HkaarDev@gmail.com>
2024-07-21 20:17:44 +0800

    Updated user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Role;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -30,6 +31,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            "role_id" => Role::first()->id,
         ];
     }
 
