10626a6d4e7ec059474d419be73859f55733aae5
Hkaar <HkaarDev@gmail.com>
2024-08-10 13:16:56 +0800

    Updated ui for genres index page

--- resources/views/genres/index.blade.php
+++ resources/views/genres/index.blade.php
@@ -1,84 +1,80 @@
-@extends('layouts.dashboard')
+@extends('layouts.app')
 
-@section('title', "Dashboard - Genres")
+@section('title', 'Dashboard - Genres')
 
-@section('content')
-<x-dashboard-side-bar selected="genre" class="bg-primary"></x-dashboard-side-bar>
+@section('main')
+  <x-dashboard-layout active="genre">
+    <div class="d-flex flex-column rounded border shadow-sm">
+      <div
+        class="d-flex align-items-center justify-content-between flex-column flex-lg-row border-body-tertiary gap-2 border-b px-3 py-4">
+        <a class="btn btn-success w-100 w-lg-fit" href="{{ route('genres.create') }}">Add a new genre</a>
 
-<div id="dashboardLeftFrame" class="flex-fill mw-100">
-  <x-dashboard-navigation selected="genres"></x-dashboard-navigation>
+        <x-query-accordion>
+          <form action="{{ route('genres.index') }}" method="get"
+            class="d-flex flex-column flex-lg-row gap-lg-1 py-lg-0 gap-2 py-3">
+            <input name="search" class="form-control" type="search" placeholder="Search"
+              value="{{ request()->query('search', '') }}" aria-label="Search">
 
-  <div class="container my-4">
-    <div class="mb-3 d-flex align-items-center justify-content-between flex-column flex-lg-row gap-2">
-      <a class="btn btn-success w-100 w-lg-fit" href="{{ route('genres.create') }}">Add a new genre</a>
+            <select name="o" class="form-select" aria-label="Default select example">
+              <option selected disabled>Order by</option>
 
-      <x-query-accordion>
-        <form action="{{ route('genres.index') }}" method="get" class="d-flex gap-2 flex-column flex-lg-row gap-lg-1 py-3 py-lg-0">
-          <input name="search" class="form-control" type="search" placeholder="Search" value="{{ request()->query('search', '') }}" aria-label="Search">
+              @if (request()->query('o') === 'oldest')
+                <option selected value="oldest">Oldest</option>
+              @else
+                <option value="oldest">Oldest</option>
+              @endif
 
-          <select name="o" class="form-select" aria-label="Default select example">
-            <option selected disabled>Order by</option>
+              @if (request()->query('o') === 'latest')
+                <option selected value="latest">Latest</option>
+              @else
+                <option value="latest">Latest</option>
+              @endif
+            </select>
 
-            @if (request()->query('o') === "oldest")
-              <option selected value="oldest">Oldest</option>
-            @else
-              <option value="oldest">Oldest</option>
-            @endif
+            <button class="btn btn-outline-primary" type="submit">Apply</button>
+          </form>
+        </x-query-accordion>
+      </div>
 
-            @if (request()->query('o') === "latest")
-              <option selected value="latest">Latest</option>
-            @else
-              <option value="latest">Latest</option>
-            @endif
-          </select>
+      <div class="table-responsive px-3">
+        <table class="table-striped table-bordered table-hover table">
+          <thead>
+            <th scope="col" width="5%">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col" width="15%">Actions</th>
+          </thead>
 
-          <button class="btn btn-outline-primary" type="submit">Apply</button>
-        </form>
-      </x-query-accordion>
+          <tbody>
+            @foreach ($genres as $i => $genre)
+              <tr>
+                <td>{{ ($genres->currentPage() - 1) * $genres->perPage() + $i+1 }}</td>
+                <td>{{ $genre->name }}</td>
+
+                <td class="d-flex gap-1">
+                  <button type="button" class="btn btn-info" data-bs-target="#detailsModal" data-bs-toggle="modal"
+                    hx-get="{{ route('genres.show', $genre->id) }}" hx-target="#detailsBody" hx-swap="innerHTML">Show
+                  </button>
+
+                  <button type="button" class="btn btn-danger" hx-confirm="Are you sure you want to delete this genre?"
+                    hx-headers='{"X-CSRF-TOKEN": "{{ csrf_token() }}"}'
+                    hx-delete="{{ route('genres.destroy', $genre->id) }}" hx-target="closest tr"
+                    hx-swap="outerHTML">Delete
+                  </button>
+
+                  <a class="btn btn-small btn-warning" href="{{ route('genres.edit', $genre->id) }}">Edit
+                  </a>
+                </td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+
+      <div class="d-flex align-items-center justify-content-between px-3 pt-2 pb-3">
+        <x-paginate-links :links="$genres" :useHtmx=false></x-paginate-links>
+
+        <x-paginate-counter :items="$genres"></x-paginate-counter>
+      </div>
     </div>
-
-    <div class="table-responsive">
-      <table class="table table-striped table-bordered table-hover">
-        <thead>
-          <th scope="col" width="5%">ID</th>
-          <th scope="col">Name</th>
-          <th scope="col" width="15%">Actions</th>
-        </thead>
-
-        <tbody>
-          @foreach ($genres as $genre)
-            <tr>
-              <td>{{ $genre->id }}</td>
-              <td>{{ $genre->name }}</td>
-
-              <td class="d-flex gap-1">
-                <button type="button" class="btn btn-info"
-                  data-bs-target="#detailsModal"
-                  data-bs-toggle="modal"
-                  hx-get="{{ route('genres.show', $genre->id) }}"
-                  hx-target="#detailsBody"
-                  hx-swap="innerHTML">Show
-                </button>
-
-                <button type="button" class="btn btn-danger"
-                  hx-confirm="Are you sure you want to delete this genre?"
-                  hx-headers='{"X-CSRF-TOKEN": "{{ csrf_token() }}"}'
-                  hx-delete="{{ route('genres.destroy', $genre->id) }}"
-                  hx-target="closest tr"
-                  hx-swap="outerHTML">Delete
-                </button>
-
-                <a class="btn btn-small btn-warning"
-                  href="{{ route('genres.edit', $genre->id) }}">Edit
-                </a>
-              </td>
-            </tr>
-          @endforeach
-        </tbody>
-      </table>
-    </div>
-
-    <x-paginate-links :links="$genres" :useHtmx=false></x-paginate-links>
-  </div>
-</div>
+  </x-dashboard-layout>
 @endsection
