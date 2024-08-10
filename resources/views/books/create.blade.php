8ccfef07270f8358d6a0391716e5f11b06b347de
Hkaar <HkaarDev@gmail.com>
2024-08-10 13:17:52 +0800

    Updated ui for books create page

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -1,16 +1,11 @@
-@extends('layouts.dashboard')
+@extends('layouts.app')
 
 @section('title', 'Dashboard - Books')
 
-@section('content')
-<x-dashboard-side-bar selected="book" class="bg-primary"></x-dashboard-side-bar>
-
-<div id="dashboardLeftFrame" class="flex-fill mw-100 d-flex flex-column">
-  <x-dashboard-navigation selected="books"></x-dashboard-navigation>
-
-  <div class="container flex-fill d-flex flex-column">
+@section('main')
+  <x-dashboard-layout active="book">
     <div class="row flex-fill mt-auto">
-      <div class="col-12 col-md-6 d-flex align-items-center justify-content-center mb-3 mb-md-0 mt-3 mt-md-0">
+      <div class="col-12 col-md-6 d-flex align-items-center justify-content-center mb-md-0 mt-md-0 mb-3 mt-3">
         <div id="preview" class="cover">
           Cover page will appear here
         </div>
@@ -18,12 +13,13 @@
 
       <div class="col-12 col-md-6 d-flex align-items-center justify-content-center">
         <div class="container">
-          <form method="POST" action="{{ route('books.store') }}" enctype="multipart/form-data" class="shadow p-3 rounded">
+          <form method="POST" action="{{ route('books.store') }}" enctype="multipart/form-data" class="rounded px-3 py-4 shadow-sm border">
             @csrf
 
             <div class="mb-3">
-              <label for="img" class="form-label">Image</label>
-              <input class="form-control" id="img" type="file" name="img" accept="image/gif, image/jpeg, image/png, image/jpg">
+              <label for="img" class="form-label fw-medium">Image</label>
+              <input class="form-control" id="img" type="file" name="img"
+                accept="image/gif, image/jpeg, image/png, image/jpg">
 
               @error('img')
                 <span>{{ $message }}</span>
@@ -31,8 +27,9 @@
             </div>
 
             <div class="mb-3">
-              <label for="name" class="form-label">Name</label>
-              <input class="form-control" id="name" type="text" name="name" value="{{ old('name') }}" required>
+              <label for="name" class="form-label fw-medium">Name</label>
+              <input class="form-control" id="name" type="text" name="name" value="{{ old('name') }}" placeholder="Enter a name"
+                required>
 
               @error('name')
                 <span>{{ $message }}</span>
@@ -40,8 +37,9 @@
             </div>
 
             <div class="mb-3">
-              <label for="isbn" class="form-label">ISBN</label>
-              <input class="form-control" id="isbn" type="text" name="isbn" value="{{ old('isbn') }}" required autofocus>
+              <label for="isbn" class="form-label fw-medium">ISBN</label>
+              <input class="form-control" id="isbn" type="text" name="isbn" value="{{ old('isbn') }}" placeholder="Enter the ISBN"
+                required autofocus>
 
               @error('isbn')
                 <span>{{ $message }}</span>
@@ -49,8 +47,9 @@
             </div>
 
             <div class="mb-3">
-              <label for="price" class="form-label">Price</label>
-              <input class="form-control" id="price" type="number" name="price" value="{{ old('price') }}" required>
+              <label for="price" class="form-label fw-medium">Price</label>
+              <input class="form-control" id="price" type="number" name="price" value="{{ old('price') }}" placeholder="Enter a price"
+                required>
 
               @error('price')
                 <span>{{ $message }}</span>
@@ -58,8 +57,9 @@
             </div>
 
             <div class="mb-3">
-              <label for="rate" class="form-label">Rate</label>
-              <input class="form-control" id="rate" type="number" name="rate" value="{{ old('rate') }}" required>
+              <label for="rate" class="form-label fw-medium">Rate</label>
+              <input class="form-control" id="rate" type="number" name="rate" value="{{ old('rate') }}" placeholder="Enter a pricing rate"
+                required>
 
               @error('rate')
                 <span>{{ $message }}</span>
@@ -67,22 +67,21 @@
             </div>
 
             <div class="mb-3">
-              <label for="desc" class="form-label">Description</label>
-              <textarea class="form-control" id="desc" name="desc">{{ old('desc') }}</textarea>
+              <label for="desc" class="form-label fw-medium">Description</label>
+              <textarea class="form-control" id="desc" name="desc" placeholder="Describe the book">{{ old('desc') }}</textarea>
 
               @error('desc')
                 <span>{{ $message }}</span>
               @enderror
             </div>
 
-            <div class="actions">
+            <div class="mt-4">
+              <button type="submit" class="btn btn-primary">Create</button>
               <a class="btn btn-danger" href="{{ route('books.index') }}">Cancel</a>
-              <button type="submit" class="btn btn-primary">Create Book</button>
             </div>
           </form>
         </div>
       </div>
     </div>
-  </div>
-</div>
+  </x-dashboard-layout>
 @endsection
