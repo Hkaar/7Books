8ff21252598028a99b851db8d574f1c72a511519
Hkaar <HkaarDev@gmail.com>
2024-08-10 13:15:57 +0800

    Updated ui for orders index, create, edit page

--- resources/views/orders/edit.blade.php
+++ resources/views/orders/edit.blade.php
@@ -1,30 +1,27 @@
-@extends('layouts.dashboard')
+@extends('layouts.app')
 
-@section('title', "Dashboard - Orders")
+@section('title', 'Dashboard - Orders')
 
-@section('content')
-<x-dashboard-side-bar selected="order" class="bg-primary"></x-dashboard-side-bar>
-
-<div id="dashboardLeftFrame" class="flex-fill mw-100 d-flex flex-column">
-  <x-dashboard-navigation selected="orders"></x-dashboard-navigation>
-
-  <div class="container flex-fill d-flex flex-column">
-    <div class="row mt-auto flex-fill">
-      <div class="col-12 col-md-6 d-md-flex align-items-center justify-content-center mb-3 mb-md-0 mt-3 mt-md-0 d-none">
-        <img src="{{ Vite::asset('resources/images/add-files.svg') }}" alt="Image not available..." class="img-fluid w-75 ratio-box">
+@section('main')
+  <x-dashboard-layout active="order">
+    <div class="row flex-fill mt-auto">
+      <div class="col-12 col-md-6 d-md-flex align-items-center justify-content-center mb-md-0 mt-md-0 d-none mb-3 mt-3">
+        <img src="{{ Vite::asset('resources/images/add-files.svg') }}" alt="Image not available..."
+          class="img-fluid w-75 ratio-box">
       </div>
 
       <div class="col-12 col-md-6 d-flex align-items-center justify-content-center">
         <div class="container">
-          <form action="{{ route('orders.update', $order->id) }}" method="POST" class="shadow p-3 rounded">
+          <form action="{{ route('orders.update', $order->id) }}" method="POST" class="rounded px-3 py-4 shadow-sm border">
             @csrf
             @method('PUT')
 
             <input type="hidden" name="items" id="items" value="{{ $items }}">
 
             <div class="mb-3">
-              <label for="username" class="form-label">Username or Email</label>
-              <input class="form-control" id="username" type="text" name="username" value="{{ $user->username }}" autofocus>
+              <label for="username" class="form-label fw-medium">Username or Email</label>
+              <input class="form-control" id="username" type="text" name="username" value="{{ $user->username }}"
+                autofocus>
 
               @error('username')
                 <span>{{ $message }}</span>
@@ -32,8 +29,9 @@
             </div>
 
             <div class="mb-3">
-              <label for="token" class="form-label">Token</label>
-              <input class="form-control" id="token" type="text" name="token" placeholder="{{ $order->token }}" autofocus>
+              <label for="token" class="form-label fw-medium">Token</label>
+              <input class="form-control" id="token" type="text" name="token" placeholder="{{ $order->token }}"
+                autofocus>
 
               @error('token')
                 <span>{{ $message }}</span>
@@ -41,8 +39,9 @@
             </div>
 
             <div class="mb-3">
-              <label for="placed_date" class="form-label">Placed Date</label>
-              <input class="form-control" id="placed_date" type="datetime-local" name="placed_date" value="{{ $order->placed_date }}" autofocus>
+              <label for="placed_date" class="form-label fw-medium">Placed Date</label>
+              <input class="form-control" id="placed_date" type="datetime-local" name="placed_date"
+                value="{{ $order->placed_date }}" autofocus>
 
               @error('placed_date')
                 <span>{{ $message }}</span>
@@ -50,8 +49,9 @@
             </div>
 
             <div class="mb-3">
-              <label for="return_date" class="form-label">Return Date</label>
-              <input class="form-control" id="return_date" type="datetime-local" name="return_date" value="{{ $order->return_date }}" autofocus>
+              <label for="return_date" class="form-label fw-medium">Return Date</label>
+              <input class="form-control" id="return_date" type="datetime-local" name="return_date"
+                value="{{ $order->return_date }}" autofocus>
 
               @error('return_date')
                 <span>{{ $message }}</span>
@@ -59,11 +59,12 @@
             </div>
 
             <div class="mb-3">
-              <label for="status_id" class="form-label">Status</label>
+              <label for="status_id" class="form-label fw-medium">Status</label>
 
               <select name="status_id" id="status_id" class="form-select">
                 @foreach ($statuses as $status)
-                  <option value="{{ $status->id }}" {{ $order->status->id === $status->id ? 'selected' : '' }}>{{ ucfirst(str_replace('_', ' ', $status->name)) }}</option>
+                  <option value="{{ $status->id }}" {{ $order->status->id === $status->id ? 'selected' : '' }}>
+                    {{ ucfirst(str_replace('_', ' ', $status->name)) }}</option>
                 @endforeach
               </select>
 
@@ -72,14 +73,10 @@
               @enderror
             </div>
 
-            <div class="d-flex align-items-md-center flex-column flex-md-row gap-1">
-              <button type="button" class="btn btn-secondary me-0 me-md-auto"
-                data-bs-target="#selectItems"
-                data-bs-toggle="modal"
-                hx-get="{{ route('books.multi-select') }}"
-                hx-target="#selectItemsBody"
-                hx-on::after-request="updateItemCards()"
-                hx-swap="innerHTML">Select Books
+            <div class="d-flex align-items-md-center flex-column flex-md-row gap-1 mt-4">
+              <button type="button" class="btn btn-secondary me-md-auto me-0" data-bs-target="#selectItems"
+                data-bs-toggle="modal" hx-get="{{ route('books.multi-select') }}" hx-target="#selectItemsBody"
+                hx-on::after-request="updateItemCards()" hx-swap="innerHTML">Select Books
               </button>
 
               <div class="d-flex gap-1">
@@ -91,6 +88,5 @@
         </div>
       </div>
     </div>
-  </div>
-</div>
+  </x-dashboard-layout>
 @endsection
