dcfea4ab024bf70a66f2ac5b0b8b4740991dfe77
Hkaar <HkaarDev@gmail.com>
2024-08-26 20:42:18 +0800

    Added testcase for artisan commands

---
+++ tests/Feature/CommandsTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Database\Seeders\TestSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CommandsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Test whether the make user command works
+     */
+    public function test_make_user(): void
+    {
+        $this->seed(TestSeeder::class);
+
+        $name = fake()->name();
+        $email = fake()->email();
+        $password = '12345678';
+        $role = 'admin';
+
+        $command = $this->artisan('make:user', [
+            'username' => $name,
+            'email' => $email,
+            'password' => $password,
+            'role' => $role,
+        ]);
+
+        $command->assertExitCode(0);
+    }
+}
