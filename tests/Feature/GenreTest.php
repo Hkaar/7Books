d9022d8e31e64630a89fe0385a2e2bb06f7b2dfb
Hkaar <HkaarDev@gmail.com>
2024-08-26 20:43:50 +0800

    Updated genre feature test case for database isolation

--- tests/Feature/GenreTest.php
+++ tests/Feature/GenreTest.php
@@ -5,17 +5,24 @@ namespace Tests\Feature;
 use App\Models\Genre;
 use App\Models\Role;
 use App\Models\User;
+use Database\Seeders\BookSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class GenreTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * Test whether the index route is working
      *
      * @test
      */
-    public function index(): void
+    public function test_dashboard_index(): void
     {
+        $this->seed();
+        $this->seed(BookSeeder::class);
+
         $user = User::factory()->create([
             'role_id' => Role::ByName('admin')->first()->id,
         ]);
@@ -30,8 +37,10 @@ class GenreTest extends TestCase
      *
      * @test
      */
-    public function create(): void
+    public function test_dashboard_create(): void
     {
+        $this->seed();
+
         $user = User::factory()->create([
             'role_id' => Role::ByName('admin')->first()->id,
         ]);
@@ -46,8 +55,10 @@ class GenreTest extends TestCase
      *
      * @test
      */
-    public function edit(): void
+    public function test_dashboard_edit(): void
     {
+        $this->seed();
+
         $user = User::factory()->create([
             'role_id' => Role::ByName('admin')->first()->id,
         ]);
@@ -64,8 +75,10 @@ class GenreTest extends TestCase
      *
      * @test
      */
-    public function show(): void
+    public function test_dashboard_show(): void
     {
+        $this->seed();
+        
         $user = User::factory()->create([
             'role_id' => Role::ByName('admin')->first()->id,
         ]);
