b184eeb3452e5b9f7baa9e9fabfa5d35dd1907a6
Hkaar <HkaarDev@gmail.com>
2024-08-26 20:41:12 +0800

    Added region seeder

---
+++ database/seeders/RegionSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Library;
+use App\Models\Region;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RegionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Region::factory()
+            ->count(10)
+            ->has(Library::factory()->count(1), 'libraries')
+            ->create();
+    }
+}
