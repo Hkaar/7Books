a68097ed78db21f5e53a5913f1c52f0778cfdc6f
Hkaar <HkaarDev@gmail.com>
2024-08-26 20:40:58 +0800

    Added region factory

---
+++ database/factories/RegionFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use DateTimeZone;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Region>
+ */
+class RegionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $timezones = DateTimeZone::listIdentifiers();
+        shuffle($timezones);
+        
+        $timezone = array_pop($timezones);
+
+        return [
+            'name' => $timezone.$this->faker->randomLetter(),
+            'desc' => $this->faker->paragraph(),
+            'timezone' => $timezone
+        ];
+    }
+}
